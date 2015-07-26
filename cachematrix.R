4ce4bee429463351668f7da939094002a508cb0f
zhuwuzhong <zhuwuzhong@hotmail.com>
2015-07-26 22:13:04 +0800

    aa
    as

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## test
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
